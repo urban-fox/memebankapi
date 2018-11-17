92bbb43fec601068e2c6fc72c7fdd199f3cd639f
Jargil Santos <urban-fox@users.noreply.github.com>
2018-11-17 23:39:46 +1300

    Memecontroller and launch url created

---
+++ MemeApi/Data/MemeApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemeApi.Models
+{
+    public class MemeApiContext : DbContext
+    {
+        public MemeApiContext (DbContextOptions<MemeApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MemeApi.Models.MemeItem> MemeItem { get; set; }
+    }
+}
--- MemeApi/Startup.cs
+++ MemeApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using MemeApi.Models;
 
 namespace MemeApi
 {
@@ -26,6 +28,9 @@ namespace MemeApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MemeApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MemeApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
